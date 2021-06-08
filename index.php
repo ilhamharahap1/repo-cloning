c15d874d1a38954d1accf10feb76c3593ae96b9b
ilhamharahap1 <ilhamharahap59@gmail.com>
2021-06-08 11:52:20 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
 <h1>remote</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum odit suscipit, facere consequuntur, magni maiores velit corporis culpa quam dolorum dolor aspernatur eligendi nulla illum tempora deserunt a harum inventore.</p>
 </body>
 </html>
